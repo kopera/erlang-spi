84115704656c08c7a6112d93b83c791c951901f1
Ali Sabil <ali.sabil@kopera.com>
2019-02-25 19:12:06 +0100

    Fix compilation errors

--- c_src/spidev_nif.c
+++ c_src/spidev_nif.c
@@ -4,8 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <limits.h>
 #include <errno.h>
+#include <string.h>
 
 #include <linux/spi/spidev.h>
 
@@ -14,7 +14,7 @@
 
 typedef struct {
     int fd;
-    bool closed;
+    bool open;
     ErlNifPid owner;
     ErlNifMonitor owner_monitor;
 } spidev_data_t;
@@ -22,7 +22,9 @@ typedef struct {
 static ERL_NIF_TERM am_ok;
 static ERL_NIF_TERM am_error;
 static ERL_NIF_TERM am_undefined;
+static ERL_NIF_TERM am_true;
 
+static ERL_NIF_TERM am_mode;
 static ERL_NIF_TERM am_cs_change;
 static ERL_NIF_TERM am_speed_hz;
 static ERL_NIF_TERM am_delay_usecs;
@@ -31,7 +33,7 @@ static ERL_NIF_TERM am_bits_per_word;
 static ErlNifResourceType* spidev_resource_type;
 
 
-static char* posix_error_to_string(posix_errno_t error) {
+static char* posix_error_to_string(int error) {
     switch (error) {
         case EACCES: return "eacces";
         case EINVAL: return "einval";
@@ -44,12 +46,12 @@ static char* posix_error_to_string(posix_errno_t error) {
     }
 }
 
-static ERL_NIF_TERM posix_error_to_tuple(ErlNifEnv *env, posix_errno_t posix_errno) {
+static ERL_NIF_TERM posix_error_to_tuple(ErlNifEnv *env, int posix_errno) {
     ERL_NIF_TERM error = enif_make_atom(env, posix_error_to_string(posix_errno));
     return enif_make_tuple2(env, am_error, error);
 }
 
-static uint8_t cast_spi_mode(uint value) {
+static uint8_t cast_spi_mode(unsigned int value) {
     switch (value) {
         case 0:
         default:
@@ -63,12 +65,12 @@ static uint8_t cast_spi_mode(uint value) {
     }
 }
 
-static int set_spi_mode(int fd, uint value) {
+static int set_spi_mode(int fd, unsigned int value) {
     uint8_t mode = cast_spi_mode(value);
     return ioctl(fd, SPI_IOC_WR_MODE, &mode);
 }
 
-static uint8_t cast_spi_bits_per_word(uint value) {
+static uint8_t cast_spi_bits_per_word(unsigned int value) {
     switch (value) {
         case 8:
         default:
@@ -78,29 +80,29 @@ static uint8_t cast_spi_bits_per_word(uint value) {
     }
 }
 
-static int set_spi_bits_per_word(int fd, uint value) {
+static int set_spi_bits_per_word(int fd, unsigned int value) {
     uint8_t bits_per_word = cast_spi_bits_per_word(value);
     return ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word);
 }
 
-static uint32_t cast_spi_speed(uint value) {
+static uint32_t cast_spi_speed(unsigned int value) {
     return (uint32_t) value;
 }
 
-static int set_spi_speed(int fd, uint value) {
+static int set_spi_speed(int fd, unsigned int value) {
     uint32_t speed = cast_spi_speed(value);
     return ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 }
 
-static uint32_t cast_spi_delay(uint value) {
+static uint32_t cast_spi_delay(unsigned int value) {
     return (uint32_t) value;
 }
 
 
 static ERL_NIF_TERM open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-    char filename[PATH_MAX];
+    char filename[4096];
 
-    if (!enif_get_string(env, argv[0], filename, sizeof(filename), ERL_NIF_LATIN1)
+    if (enif_get_string(env, argv[0], filename, sizeof(filename), ERL_NIF_LATIN1) <= 0
         || !enif_is_map(env, argv[1])) {
         return enif_make_badarg(env);
     }
@@ -118,19 +120,23 @@ static ERL_NIF_TERM open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]
     ERL_NIF_TERM option;
     if (enif_get_map_value(env, options, am_mode, &option)) { // mode
         unsigned int mode;
-        if (!enif_get_uint(option, &mode) || set_spi_mode(fd, mode) < 0) {
+        if (!enif_get_uint(env, option, &mode) || set_spi_mode(fd, mode) < 0) {
             close(fd);
             return enif_make_badarg(env);
         }
-    } else if (enif_get_map_value(env, options, am_speed_hz, &option)) { // speed_hz
+    }
+
+    if (enif_get_map_value(env, options, am_speed_hz, &option)) { // speed_hz
         unsigned int speed_hz;
-        if (!enif_get_uint(option, &speed_hz) || set_spi_speed(fd, speed_hz) < 0) {
+        if (!enif_get_uint(env, option, &speed_hz) || set_spi_speed(fd, speed_hz) < 0) {
             close(fd);
             return enif_make_badarg(env);
         }
-    } else if (enif_get_map_value(env, options, am_bits_per_word, &option) { // bits_per_word
+    }
+
+    if (enif_get_map_value(env, options, am_bits_per_word, &option)) { // bits_per_word
         unsigned int bits_per_word;
-        if (!enif_get_uint(option, &bits_per_word) || set_spi_bits_per_word(bits_per_word) < 0) {
+        if (!enif_get_uint(env, option, &bits_per_word) || set_spi_bits_per_word(fd, bits_per_word) < 0) {
             close(fd);
             return enif_make_badarg(env);
         }
@@ -141,7 +147,7 @@ static ERL_NIF_TERM open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]
 
     spidev_data_t* d = (spidev_data_t*) enif_alloc_resource(spidev_resource_type, sizeof(spidev_data_t));
     d->fd = fd;
-    d->closed = false;
+    d->open = true;
     d->owner = owner;
 
     if (enif_monitor_process(env, d, &owner, &d->owner_monitor)) {
@@ -150,8 +156,8 @@ static ERL_NIF_TERM open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]
         return posix_error_to_tuple(env, EINVAL);
     }
 
-    result = enif_make_resource(env, d);
-    enif_release_resource(result);
+    ERL_NIF_TERM result = enif_make_resource(env, d);
+    enif_release_resource(d);
 
     return enif_make_tuple2(env, am_ok, result);
 }
@@ -168,7 +174,8 @@ static ERL_NIF_TERM transfer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
     unsigned transfer_list_length = 0;
     enif_get_list_length(env, transfer_list, &transfer_list_length);
 
-    struct spi_ioc_transfer transfer_buffer[transfer_list_length] = {0};
+    struct spi_ioc_transfer transfer_buffer[transfer_list_length];
+    memset(transfer_buffer, 0, transfer_list_length * sizeof(struct spi_ioc_transfer));
 
     ERL_NIF_TERM transfer;
     ERL_NIF_TERM rest;
@@ -176,42 +183,48 @@ static ERL_NIF_TERM transfer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
     unsigned index = 0;
     while (enif_get_list_cell(env, transfer_list, &transfer, &rest)) {
         ErlNifBinary tx;
-        ErlNifBinary rx;
+        ERL_NIF_TERM rx;
 
         if (enif_is_tuple(env, transfer)) {
             int arity;
-            ERL_NIF_TERM fields[];
+            const ERL_NIF_TERM* fields;
 
             enif_get_tuple(env, transfer, &arity, &fields);
             if (arity == 2
                     && enif_inspect_binary(env, fields[0], &tx)
                     && enif_is_map(env, fields[1])) {
 
-                unsigned char* rx_buffer = enif_make_new_binary(env, tx->size, &rx);
-                transfer_buffer[index].tx_buf = (unsigned long)tx->data;
+                unsigned char* rx_buffer = enif_make_new_binary(env, tx.size, &rx);
+                transfer_buffer[index].tx_buf = (unsigned long)tx.data;
                 transfer_buffer[index].rx_buf = (unsigned long)rx_buffer;
-                transfer_buffer[index].len = tx->size;
+                transfer_buffer[index].len = tx.size;
 
                 ERL_NIF_TERM option;
                 if (enif_get_map_value(env, fields[1], am_cs_change, &option)) { // cs_change
                     transfer_buffer[index].cs_change = (enif_compare(option, am_true) == 0);
-                } else if (enif_get_map_value(env, fields[1], am_speed_hz, &option)) { // speed_hz
+                }
+
+                if (enif_get_map_value(env, fields[1], am_speed_hz, &option)) { // speed_hz
                     unsigned int speed_hz;
-                    if (enif_get_uint(option, &speed_hz)) {
+                    if (enif_get_uint(env, option, &speed_hz)) {
                         transfer_buffer[index].speed_hz = cast_spi_speed(speed_hz);
                     } else {
                         return enif_make_badarg(env);
                     }
-                } else if (enif_get_map_value(env, fields[1], am_delay_usecs, &option) { // delay_usecs
+                }
+
+                if (enif_get_map_value(env, fields[1], am_delay_usecs, &option)) { // delay_usecs
                     unsigned int delay_usecs;
-                    if (enif_get_uint(option, &delay_usecs)) {
+                    if (enif_get_uint(env, option, &delay_usecs)) {
                         transfer_buffer[index].delay_usecs = cast_spi_delay(delay_usecs);
                     } else {
                         return enif_make_badarg(env);
                     }
-                } else if (enif_get_map_value(env, fields[1], am_bits_per_word, &option) { // bits_per_word
+                }
+
+                if (enif_get_map_value(env, fields[1], am_bits_per_word, &option)) { // bits_per_word
                     unsigned int bits_per_word;
-                    if (enif_get_uint(option, &bits_per_word)) {
+                    if (enif_get_uint(env, option, &bits_per_word)) {
                         transfer_buffer[index].bits_per_word = cast_spi_bits_per_word(bits_per_word);
                     } else {
                         return enif_make_badarg(env);
@@ -223,11 +236,11 @@ static ERL_NIF_TERM transfer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
                 return enif_make_badarg(env);
             }
         } else if (enif_inspect_binary(env, transfer, &tx)) {
-            unsigned char* rx_buffer = enif_make_new_binary(env, tx->size, &rx);
+            unsigned char* rx_buffer = enif_make_new_binary(env, tx.size, &rx);
 
-            transfer_buffer[index].tx_buf = (unsigned long)tx->data;
+            transfer_buffer[index].tx_buf = (unsigned long)tx.data;
             transfer_buffer[index].rx_buf = (unsigned long)rx_buffer;
-            transfer_buffer[index].len = tx->size;
+            transfer_buffer[index].len = tx.size;
 
             rx_list = enif_make_list_cell(env, rx, rx_list);
         } else {
@@ -242,7 +255,9 @@ static ERL_NIF_TERM transfer_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
         return posix_error_to_tuple(env, errno);
     }
 
-    return enif_make_tuple2(env, am_ok, rx_list);
+    ERL_NIF_TERM result;
+    enif_make_reverse_list(env, rx_list, &result);
+    return enif_make_tuple2(env, am_ok, result);
 }
 
 static ERL_NIF_TERM close_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
@@ -252,7 +267,7 @@ static ERL_NIF_TERM close_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
         return enif_make_badarg(env);
     }
 
-    if (!d->closed) {
+    if (d->open) {
         if (enif_select(env, d->fd, ERL_NIF_SELECT_STOP, d, NULL, am_undefined) >= 0) {
             return am_ok;
         }
@@ -262,9 +277,9 @@ static ERL_NIF_TERM close_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
 }
 
 static ErlNifFunc nif_funcs[] = {
-    {"open", 2, open_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
-    {"transfer", 2, transfer_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
-    {"close", 1, close_nif, ERL_NIF_DIRTY_JOB_IO_BOUND}
+    {"open_nif", 2, open_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
+    {"transfer_nif", 2, transfer_nif, ERL_NIF_DIRTY_JOB_IO_BOUND},
+    {"close_nif", 1, close_nif, ERL_NIF_DIRTY_JOB_IO_BOUND}
 };
 
 static void on_stop(ErlNifEnv* env, void* obj, int fd, int is_direct_call)
@@ -288,12 +303,12 @@ static void on_owner_down(ErlNifEnv* env, void* obj, ErlNifPid* pid, ErlNifMonit
 
 static int on_load(ErlNifEnv *env, void** priv_data, ERL_NIF_TERM load_info)
 {
-    ErlNifResourceTypeInit callbacks;
-
     am_ok = enif_make_atom(env, "ok");
     am_error = enif_make_atom(env, "error");
     am_undefined = enif_make_atom(env, "undefined");
+    am_true = enif_make_atom(env, "true");
 
+    am_mode = enif_make_atom(env, "mode");
     am_cs_change = enif_make_atom(env, "cs_change");
     am_speed_hz = enif_make_atom(env, "speed_hz");
     am_delay_usecs = enif_make_atom(env, "delay_usecs");
@@ -305,7 +320,7 @@ static int on_load(ErlNifEnv *env, void** priv_data, ERL_NIF_TERM load_info)
         .down = on_owner_down,
     };
     spidev_resource_type = enif_open_resource_type_x(env, "spidev",
-        spidev_resource_type_callbacks, ERL_NIF_RT_CREATE, NULL);
+        &spidev_resource_type_callbacks, ERL_NIF_RT_CREATE, NULL);
 
     *priv_data = NULL;
 
@@ -325,10 +340,10 @@ static int on_upgrade(ErlNifEnv *env, void** priv_data, void** old_priv_data, ER
     if (*priv_data != NULL) {
         return -1; /* Don't know how to do that */
     }
-    if (load(env, priv_data, load_info)) {
+    if (on_load(env, priv_data, load_info)) {
         return -1;
     }
     return 0;
 }
 
-ERL_NIF_INIT(spidev, nif_funcs, on_load, NULL, on_upgrade, on_unload);
+ERL_NIF_INIT(spi, nif_funcs, on_load, NULL, on_upgrade, on_unload);
--- src/spi.erl
+++ src/spi.erl
@@ -19,24 +19,39 @@
 
 
 -spec open(Filename) -> {ok, device()} | {error, term()} when
-    Filename :: string().
+    Filename :: string() | binary().
 open(Filename) ->
     open(Filename, #{}).
 
 
 -spec open(Filename, Options) -> {ok, device()} | {error, term()} when
-    Filename :: string(),
+    Filename :: string() | binary(),
     Options :: #{
         mode => 0 | 1 | 2 | 3,
         speed_hz => pos_integer(),
         bits_per_word => 8 | 16
     }.
-open(_Filename, _Options) ->
+open(Filename, Options) when is_binary(Filename) ->
+    open(unicode:characters_to_list(Filename), Options);
+
+open(Filename, Options) when is_list(Filename) ->
+    open_nif(Filename, maps:merge(#{
+        mode => 0,
+        speed_hz => 1000,
+        bits_per_word => 8
+    }, Options)).
+
+%% nif
+open_nif(_Filename, _Options) ->
     {error, unsupported}.
 
 
 -spec close(device()) -> ok | {error, term()}.
-close(_Device) ->
+close(Device) ->
+    close_nif(Device).
+
+%% nif
+close_nif(_Device) ->
     {error, unsupported}.
 
 
@@ -48,7 +63,11 @@ close(_Device) ->
     delay_usecs => pos_integer(),
     cs_change => boolean()
 }.
-transfer(_Device, _Transfers) ->
+transfer(Device, Transfers) ->
+    transfer_nif(Device, Transfers).
+
+%% nif
+transfer_nif(_Device, _Transfers) ->
     {error, unsupported}.
 
 
@@ -74,7 +93,7 @@ read(Device, Size) ->
 
 -spec read(device(), pos_integer(), transfer_options()) -> {ok, binary()} | {error, term()}.
 read(Device, Size, Options) ->
-    case transfer(Device, [{<<0:Size/binary>>, Options}]) of
+    case transfer(Device, [{<<0:(Size * 8)>>, Options}]) of
         {ok, [Reply]} when byte_size(Reply) =:= Size ->
             {ok, Reply};
         {error, _} = Error ->
@@ -108,7 +127,7 @@ nif_path() ->
 
 
 nif_path({unix, linux}, Dir) ->
-    filename:join([Dir, "port"]);
+    filename:join([Dir, "spidev_nif"]);
 
 nif_path(_, _Dir) ->
     undefined.
